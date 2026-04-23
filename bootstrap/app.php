604ca8c1f423e968845e9821dd5039b65c9c25fe
Lfrty <matiasdelosriosaguilar@gmail.com>
2026-04-23 23:50:44 +0200

    Se personalizan errores clásicos

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -14,6 +14,44 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withMiddleware(function (Middleware $middleware): void {
         //
     })
-    ->withExceptions(function (Exceptions $exceptions): void {
-        //
-    })->create();
+     ->withExceptions(function (Exceptions $exceptions) {
+
+        $exceptions->render(function (Throwable $e, $request) {
+
+            // Si es API (JSON)
+            if ($request->expectsJson()) {
+
+                $status = $e instanceof HttpExceptionInterface
+                    ? $e->getStatusCode()
+                    : 500;
+
+                $mensajes = [
+                    404 => 'Recurso no encontrado',
+                    403 => 'No tienes permisos para acceder a esto',
+                    500 => 'Error interno del servidor',
+                ];
+
+                return response()->json([
+                    'ok' => false,
+                    'error' => $mensajes[$status] ?? 'Error inesperado',
+                ], $status);
+            }
+
+            // Web normal (HTML)
+            $status = $e instanceof HttpExceptionInterface
+                ? $e->getStatusCode()
+                : 500;
+
+            return response()->view('errors.custom', [
+                'mensaje' => match ($status) {
+                    404 => 'Lo que buscas no existe.',
+                    403 => 'No tienes permiso para ver esto.',
+                    500 => 'Algo explotó dentro del servidor.',
+                    default => 'Error inesperado.',
+                },
+                'codigo' => $status
+            ], $status);
+        });
+
+    })
+    ->create();
