f182b8c390583b8ae579f5be1fbf535457597224
Lfrty <matiasdelosriosaguilar@gmail.com>
2026-04-25 01:18:03 +0200

    Se añaden endpoints con usuario logueado

--- routes/api.php
+++ routes/api.php
@@ -7,4 +7,32 @@ Route::get('/test', function () {
     return response()->json(['ok' => true]);
 });
 
+// Opciones Usuario
+
+// Registro
 Route::post('/registrar', [AuthController::class, 'registrar']);
+
+// Login
+Route::post('/login', [AuthController::class, 'login']);
+
+// Opciones logueado
+Route::middleware('auth:sanctum')->group(function () {
+
+    // Cerrar sesión
+    Route::post('/logout', [AuthController::class, 'logout']);
+
+    // Perfil
+    Route::get('/user', fn (Request $request) => $request->user());
+
+    // Ver Listas
+    Route::get('/listas', [ListaController::class, 'index']);
+
+});
+
+Route::post('/listas', [ListaController::class, 'store']);
+
+Route::delete('/listas/{id}', [ListaController::class, 'destroy']); // soft delete
+
+
+Route::delete('/listas/{id}/force', [ListaController::class, 'forceDelete']);
+Route::post('/listas/{id}/restore', [ListaController::class, 'restore']);
